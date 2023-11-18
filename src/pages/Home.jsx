86ce0420677ce504e532d3193a29d35f25a1a867
Erickype <erickype@hotmail.com>
2023-11-18 17:15:01 -0500

    and glft file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import Loader from '@react-three/fiber'
+import Loader from '../components/Loader'
+import Island from '../models/Island'
 
 const Home = () => {
   return (
@@ -10,7 +11,14 @@ const Home = () => {
       </div>
       <Canvas className='w-full h-screen bg-transparent'
         camera={{ near: 0.1, far: 1000 }}>
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+          <Island />
+        </Suspense>
       </Canvas>
     </section>
   )
