cbb45678161e70db62e281c92ca064607142e23d
Erickype <erickype@hotmail.com>
2023-11-19 20:28:44 -0500

    Directional ligth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
       <Canvas className='w-full h-screen bg-transparent'
         camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
-          <directionalLight />
+          <directionalLight position={[1,1,1]} intensity={2}/>
           <ambientLight />
           <pointLight />
           <spotLight />
